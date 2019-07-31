c5fcf7566e5d2b414f5b029f89a435b374b7f3b5
Hector Priori <hectorpriori@gmail.com>
2019-07-31 14:14:14 -0300

    servicio funcionando desde manifest.json agregado archivo idioma es_ES Master controller y view limpio

--- WebContent/controller/Master.controller.js
+++ WebContent/controller/Master.controller.js
@@ -1,134 +1,43 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
 	"sap/ui/model/json/JSONModel",
-	"../model/formatter",
 	"sap/ui/model/Filter",
 	"sap/ui/model/FilterOperator",
-	"sap/ui/model/odata/v2/ODataModel"
-], function(Controller, JSONModel, formatter, Filter, FilterOperator, ODataModel) {
+	"sap/m/MessageToast"
+], function(Controller, JSONModel, Filter, FilterOperator, MessageToast) {
 	"use strict";
 
 	return Controller.extend("terragene.aprobentrega.controller.InvoiceList", {
 
-		formatter: formatter,
-		
 		onInit : function() {
 			var list = this.getView().byId("idList2");
-			//Load OData Service
-			//var sURL = "http://localhost:8081/https://dev.terragene.com.ar/sap/opu/odata/sap/zsd_entrega_aprob_1_srv/";
-			var sURL = "https://dev.terragene.com.ar/sap/opu/odata/sap/zsd_entrega_aprob_1_srv/";
-			
-				var oDataModel = new ODataModel(sURL, {
-				     //user : "rjerez",
-				     //password : "Pipobi2018",
-				     json : true,
-				     useBatch: true,
-				     headers : {
-				    	 Authorization : "Basic cmplcmV6OlBpcG9iaTIwMTg="
-				     }
-				});
-			sap.ui.getCore().setModel(oDataModel);
-			
+
 			var oJsonModel = new JSONModel();
+			
+			var oDataModel = this.getOwnerComponent().getModel("invoice")
 			
 			var servicioUrl = "/Entrega_cabSet?"
 			oDataModel.read(servicioUrl, {
 				success : function(data) {
 					//Read output
-					alert(data);
-					alert(data.results);
 					var result = data.results;
 				
 					//set JSONoutput to a JSONModel
 					oJsonModel.setData({
-						listItems : result
+						listaEntregas : result
 					});
 					
 					list.setModel(oJsonModel);
 				}, 
 				error : function(err) {
-					alert(err);
-					alert(err.message);
 					var errTxt = err.message + "\n" + err.request.requestUri;
-					sap.m.MessageBox.show(errTxt, sap.m.MessageBox.Icon.ERROR, "Service Error");
+					MessageBox.show(errTxt, MessageBox.Icon.ERROR, "Service Error");
 				}
 			});
 			
 			sap.ui.getCore().setModel(oJsonModel, "listItems")
-			
-			
-/*			//Call OdataService
-			oDataModel.read(servicioUrl, function(data) {
-
-				//Read output
-				var result = data.results;
-				
-				//set JSONoutput to a JSONModel
-				var oModel = new JSONModel();
-				oModel.setData({
-					listItems : result
-				});
-				
-				//Set output to ListControl			
-				list.setModel(oModel);
-							
-			}, function(err) {
-				var errTxt = err.message + "\n" + err.request.requestUri;
-			});	
-*/
-			
-			
-			
-/*
-			var sURL = "http://localhost:8081/https://dev.terragene.com.ar/sap/opu/odata/sap/zsd_entrega_aprob_1_srv/Entrega_cab"
-			
-			var oDataModel = new ODataModel(sURL, {
-			     user : "rjerez",
-			     password : "Pipobi2018",
-			     headers : {
-			    	 Authorization : "Basic cmplcmV6OlBpcG9iaTIwMTg="
-			     }
-			});
-			this.getView().setModel(oDataModel, "oDataModel");
-			
-			var oViewModel = new JSONModel({
-				currency : "EUR"
-			});
-			this.getView().setModel(oViewModel, "view");
-*/
 		},
-/*		
-		pressODataSrv: function(){		
-			
-			//Get list control reference
-			var list = this.getView().byId("idList1");
-			
-			//Frame Url with EntitySet
-			//var url = serviceUrl + "Entrega_cab";
-			var url = "http://localhost:8081/https://dev.terragene.com.ar/sap/opu/odata/sap/zsd_entrega_aprob_1_srv/Entrega_cab";
-					
-			//Call OdataService
-			OData.read(url, function(data) {
-
-				//Read output
-				var result = data.results;
-				
-				//set JSONoutput to a JSONModel
-				var oModel = new JSONModel();
-				oModel.setData({
-					listItems : result
-				});
-				
-				//Set output to ListControl			
-				list.setModel(oModel);
-							
-			}, function(err) {
-				var errTxt = err.message + "\n" + err.request.requestUri;
-				sap.m.MessageBox.show(errTxt, sap.m.MessageBox.Icon.ERROR, "Service Error");
-			});	
-		},
-
-*/
+		
 		onFilterInvoices : function(oEvent) {
 			// build filter array
 			var aFilter = [];
